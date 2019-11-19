33a59449f228478e8873f89ad96822767e2c7838
Hieu <45428745+hieukut456@users.noreply.github.com>
2019-11-20 00:28:30 +0700

    Add files via upload

--- Presentation/Pages/Index.cshtml.cs
+++ Presentation/Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
 
 namespace Presentation.Pages
 {
@@ -12,6 +13,14 @@ namespace Presentation.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        [BindProperty]
+        public string Flight_type{ get; set; }
+        [BindProperty]
+        public string Checkin_date{ get; set; }
+        [BindProperty]
+        public string Checkout_date { get; set; }
+
+        public string Msg { get; set; }
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -19,7 +28,31 @@ namespace Presentation.Pages
 
         public void OnGet()
         {
-
+            
         }
+        // public IActionResult OnPostFlightSearch()
+        // {
+        //     if(Flight_type.Equals("round"))
+        //     {
+        //         if(Checkout_date != null )
+        //         {
+        //             HttpContext.Session.SetString("checkin_date", Checkin_date);
+        //             HttpContext.Session.SetString("checkout_date", Checkout_date); 
+        //             return RedirectToPage("Flight");    
+        //         }
+        //         else
+        //         {
+        //             Msg = "Please choose check out date";
+        //             return Page();            
+        //         }
+                
+        //     }
+        //     if(Flight_type.Equals("one"))
+        //     {
+        //         HttpContext.Session.SetString("checkin_date",Checkin_date);
+        //         return RedirectToPage("Flight");
+        //     }
+            
+        // }
     }
 }
